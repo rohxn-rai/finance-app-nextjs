89596a67990b779a8212dcea84ef00a5cfb5a6d1
rohxn-rai <rohan.rai932@outlook.com>
2025-08-11 06:59:39 -0400

    changed modified component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/components/header";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import TransactionItem from "@/components/transaction-item";
 import Trend from "@/components/Trend";
 
