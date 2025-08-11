d52088751b7fbe98370bb4a145f71e747fc412c0
rohxn-rai <rohan.rai932@outlook.com>
2025-08-11 06:43:32 -0400

    added TransactionItem component

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import TransactionItem from "@/components/transaction-item";
 import Trend from "@/components/Trend";
 
 const PlaygroundPage = () => {
@@ -22,6 +23,30 @@ const PlaygroundPage = () => {
           <Trend type="Expense" amount={12000} prevAmount={10000} />
           <Trend type="Investment" amount={7000} prevAmount={11100} />
           <Trend type="Saving" amount={500} prevAmount={950} />
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Transaction Item</h2>
+        <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+        <div className="flex flex-col gap-4">
+          <TransactionItem type="Income" description="Salary" amount={2000} />
+          <TransactionItem
+            type="Expense"
+            category="Food"
+            description="Going out to eat"
+            amount={29}
+          />
+          <TransactionItem
+            type="Saving"
+            description="For children"
+            amount={500}
+          />
+          <TransactionItem
+            type="Investment"
+            description="In Microsoft"
+            amount={9000}
+          />
         </div>
       </div>
     </main>
