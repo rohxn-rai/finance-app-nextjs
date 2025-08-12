ac28a0381b89618bba4f69e8a801c5a9b3dc1892
rohxn-rai <rohan.rai932@outlook.com>
2025-08-12 04:39:30 -0400

    added more config to buttons - size options different text size for each size buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -54,8 +54,8 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         xs: "h-6 rounded-md gap-1 px-2 has-[>svg]:px-2 text-xs",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 text-sm",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4 text-lg",
         icon: "size-9",
       },
     },
