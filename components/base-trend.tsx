8f2f5ae6fccffe9d36f67ebe9d7eb4d9e0f782a1
rohxn-rai <rohan.rai932@outlook.com>
2025-08-12 10:19:22 -0400

    shifted trend into base-trend

---
+++ components/base-trend.tsx
@@ -0,0 +1,52 @@
+import useFormatCurrency from "@/hooks/use-format-currency";
+import { ArrowDownLeft, ArrowUpRight } from "lucide-react";
+import { useMemo } from "react";
+
+const BaseTrend = ({
+  type,
+  amount,
+  prevAmount,
+}: {
+  type: "Income" | "Expense" | "Investment" | "Saving";
+  amount: number;
+  prevAmount?: number;
+}) => {
+  const colorClasses = {
+    Income: "text-green-700 dark:text-green-300",
+    Expense: "text-red-700 dark:text-red-300",
+    Investment: "text-indigo-700 dark:text-indigo-300",
+    Saving: "text-yellow-700 dark:text-yellow-300",
+  };
+
+  const calcPercentageChange = (amount: number, prevAmount?: number) => {
+    if (!prevAmount || !amount) return 0;
+    return ((amount - prevAmount) / prevAmount) * 100;
+  };
+
+  const percentageChange = useMemo(
+    () => calcPercentageChange(amount, prevAmount),
+    [amount, prevAmount]
+  );
+
+  const formattedAmount = useFormatCurrency(amount);
+
+  return (
+    <div>
+      <div className={`font-semibold ${colorClasses[type]}`}>{type}</div>
+      <div className="text-2xl font-semibold text-black dark:text-white mb-2">
+        {formattedAmount}
+      </div>
+      <div className="flex flex-row gap-1 items-center text-sm">
+        {percentageChange <= 0 && (
+          <ArrowDownLeft className="text-red-700 dark:text-red-300" />
+        )}
+        {percentageChange > 0 && (
+          <ArrowUpRight className="text-green-700 dark:text-green-300" />
+        )}
+        {percentageChange.toFixed(0)}% vs last period
+      </div>
+    </div>
+  );
+};
+
+export default BaseTrend;
