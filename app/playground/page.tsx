88a77a55a4bdbe2cc7667ccda7ea393e1d4bbfc5
rohxn-rai <rohan.rai932@outlook.com>
2025-08-12 04:40:38 -0400

    added more components - to playground   - added buttons, form fields and checkboxes

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,16 +1,29 @@
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
 import Header from "@/components/header";
 import TransactionItem from "@/components/transaction-item";
 import TransactionSummaryItem from "@/components/transaction-summary-item";
 import Trend from "@/components/Trend";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const PlaygroundPage = () => {
   return (
-    <main className="flex flex-col gap-8">
+    <main className="flex flex-col mb-44 gap-8">
       <h1 className="text-4xl mt-8">Playground Page</h1>
 
       <div>
         <h2 className="mb-4 text-lg font-mono">Page Header</h2>
-        <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
         <div>
           <Header />
         </div>
@@ -18,7 +31,7 @@ const PlaygroundPage = () => {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">Trend</h2>
-        <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
         <div className="flex flex-row gap-8">
           <Trend type="Income" amount={1000} prevAmount={900} />
           <Trend type="Expense" amount={12000} prevAmount={10000} />
@@ -29,7 +42,7 @@ const PlaygroundPage = () => {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">Transaction Item</h2>
-        <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
         <div className="flex flex-col gap-4">
           <TransactionItem type="Income" description="Salary" amount={2000} />
           <TransactionItem
@@ -55,10 +68,10 @@ const PlaygroundPage = () => {
         <h2 className="mb-4 text-lg font-mono">
           Transaction Summary Item + Transaction Item
         </h2>
-        <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
         <div className="flex flex-col gap-4">
-          <TransactionSummaryItem date="2025-08-11" amount={1000} />
-          <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+          <TransactionSummaryItem date="2025-08-11" amount={3500} />
+          <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
           <TransactionItem type="Income" description="Salary" amount={2000} />
           <TransactionItem
             type="Expense"
@@ -79,10 +92,126 @@ const PlaygroundPage = () => {
         </div>
       </div>
 
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Button</h2>
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
+        <div className="flex flex-col gap-4">
+          <h3>Variants</h3>
+          <div className="flex flex-row gap-4 h-16 items-center">
+            <Button>Hello</Button>
+            <Button variant="outline">Hello</Button>
+            <Button variant="ghost">Hello</Button>
+            <Button variant="destructive">Hello</Button>
+            <Button variant="secondary">Hello</Button>
+            <Button variant="link">Hello</Button>
+          </div>
+          <h3>Sizes</h3>
+          <div className="flex flex-row gap-4 h-16 items-center">
+            <Button size="xs">Hello</Button>
+            <Button size="sm">Hello</Button>
+            <Button>Hello</Button>
+            <Button size="lg">Hello</Button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Form</h2>
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex flex-col gap-1">
+            <Label htmlFor="name">Your Name</Label>
+            <Input id="name" type="text" placeholder="Enter your name ..." />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <Label>City</Label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select your city ..." />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="New Delhi" className="bg-transparent">
+                  New Delhi
+                </SelectItem>
+                <SelectItem value="Mumbai" className="bg-transparent">
+                  Mumbai
+                </SelectItem>
+                <SelectItem value="Jaipur" className="bg-transparent">
+                  Jaipur
+                </SelectItem>
+                <SelectItem value="Kolkata" className="bg-transparent">
+                  Kolkata
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Checkbox</h2>
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-row gap-2 ml-3">
+            <Checkbox id="check-box-unchecked" className="cursor-pointer" />
+            <Label htmlFor="check-box-unchecked" className="cursor-pointer">
+              Option (unchecked)
+            </Label>
+          </div>
+          <div className="flex flex-row gap-2 ml-3">
+            <Checkbox
+              id="check-box-checked"
+              className="cursor-pointer"
+              defaultChecked
+            />
+            <Label htmlFor="check-box-checked" className="cursor-pointer">
+              Option (checked)
+            </Label>
+          </div>
+          <div className="flex flex-row gap-2 ml-3">
+            <Checkbox id="check-box-disabled" disabled />
+            <Label htmlFor="check-box-disabled">Option (disabled)</Label>
+          </div>
+          <Label
+            htmlFor="wrapped-check-box"
+            className={cn(
+              "hover:bg-accent/50 flex items-start ",
+              "gap-3 rounded-lg border p-3 ml-0",
+              "has-[[aria-checked=true]]:border-blue-600 ",
+              "has-[[aria-checked=true]]:bg-blue-50 ",
+              "dark:has-[[aria-checked=true]]:border-blue-900 ",
+              "dark:has-[[aria-checked=true]]:bg-blue-950",
+              "cursor-pointer"
+            )}
+          >
+            <Checkbox
+              id="wrapped-check-box"
+              defaultChecked
+              className={cn(
+                "data-[state=checked]:border-blue-600 ",
+                "data-[state=checked]:bg-blue-600 ",
+                "data-[state=checked]:text-white ",
+                "dark:data-[state=checked]:border-blue-700 ",
+                "dark:data-[state=checked]:bg-blue-700"
+              )}
+            />
+            <div className="grid gap-1.5 font-normal">
+              <p className="text-sm leading-none font-medium">
+                Enable notifications
+              </p>
+              <p className="text-muted-foreground text-sm">
+                You can enable or disable notifications at any time.
+              </p>
+            </div>
+          </Label>
+        </div>
+      </div>
+
       {/*
        <div>
         <h2 className="mb-4 text-lg font-mono">Component</h2>
-        <hr className="mb-4 border-gray-200 dark:border-y-gray-800" />
+        <hr className="mb-8 border-gray-200 dark:border-y-gray-800" />
         <div className="flex flex-row gap-8">
           <Component />
           <Component />
