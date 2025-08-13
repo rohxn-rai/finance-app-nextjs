5fbf7f9ce6721ba5fff681dfdddbfcad49b2cc35
rohxn-rai <rohan.rai932@outlook.com>
2025-08-13 04:48:18 -0400

    added the skeleton to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,19 @@
-import TransactionItemLoadingSkeleton from "@/components/skeletons/transactions/transaction-list-loading-skeleton";
+import TransactionItemLoadingSkeleton
+  from "@/components/skeletons/transactions/transaction-list-loading-skeleton";
 import TransactionList from "@/components/transaction-list";
-import Trend from "@/components/trend";
+import TrendSetLoadingSkeleton
+  from "@/components/skeletons/trend/trend-set-loading-skeleton";
 
-import { Suspense } from "react";
+import {Suspense} from "react";
 
 const DashboardPage = () => {
   return (
     <>
       <div className="flex flex-col gap-8">
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-          <Trend type="Income" />
-          <Trend type="Expense" />
-          <Trend type="Investment" />
-          <Trend type="Saving" />
-        </section>
-      <Suspense fallback={<TransactionItemLoadingSkeleton />}>
-        <TransactionList />
-      </Suspense>
+        <TrendSetLoadingSkeleton/>
+        <Suspense fallback={<TransactionItemLoadingSkeleton/>}>
+          <TransactionList/>
+        </Suspense>
       </div>
     </>
   );
