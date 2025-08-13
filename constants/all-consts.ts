0ad10d193560ce669be6b761ed505c4107ba8463
rohxn-rai <rohan.rai932@outlook.com>
2025-08-13 09:38:38 -0400

    added constants file

---
+++ constants/all-consts.ts
@@ -0,0 +1,15 @@
+import { CategoryOfTransaction, TypeOfTransaction } from "@/types/transaction";
+
+export const TYPEOFTRANSACTION : TypeOfTransaction[] = [
+  "Income",
+  "Expense",
+  "Investment",
+  "Saving" ]
+
+export const CATEGORYOFTRANSACTION : CategoryOfTransaction[] = [
+  "Housing",
+  "Transport",
+  "Health",
+  "Food",
+  "Education",
+  "Other" ]
