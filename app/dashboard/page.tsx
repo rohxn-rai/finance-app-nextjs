ca7a45c91ab22e6e8c49bcc11f85f537841cf176
rohxn-rai <rohan.rai932@outlook.com>
2025-08-13 05:40:45 -0400

    added add transaction page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,33 @@
+import { Suspense } from "react";
+import Link from "next/link";
+
 import TransactionItemLoadingSkeleton
   from "@/components/skeletons/transactions/transaction-list-loading-skeleton";
 import TransactionList from "@/components/transaction/transaction-list";
 import TrendSetLoadingSkeleton
   from "@/components/skeletons/trend/trend-set-loading-skeleton";
+import { Button } from "@/components/ui/button";
 
-import {Suspense} from "react";
+import { PlusCircle } from "lucide-react";
 
 const DashboardPage = () => {
   return (
     <>
       <div className="flex flex-col gap-8">
+        <section>
+          <h1 className="text-4xl font-semibold">Summary</h1>
+        </section>
         <TrendSetLoadingSkeleton/>
-        <Suspense fallback={<TransactionItemLoadingSkeleton/>}>
+        <section className="flex flex-row justify-between items-center">
+          <h2 className="text-2xl">Transactions</h2>
+          <Link href="/dashboard/transaction/add">
+            <Button variant="outline" size="sm" className="cursor-pointer">
+              <PlusCircle className="w-4 h-4"/>
+              <span>Add</span>
+            </Button>
+          </Link>
+        </section>
+        <Suspense fallback={ <TransactionItemLoadingSkeleton/> }>
           <TransactionList/>
         </Suspense>
       </div>
---
+++ app/dashboard/transaction/add/page.tsx
@@ -0,0 +1,9 @@
+const AddTransactionPage = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold mb-8">Add Transaction</h1>
+    </>
+  )
+}
+
+export default AddTransactionPage
