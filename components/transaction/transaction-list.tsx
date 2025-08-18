5c27ce76afe0728538621d8e189eb850368bc4bd
rohxn-rai <rohan.rai932@outlook.com>
2025-08-18 04:25:54 -0400

    updated dummy database

--- components/transaction/transaction-list.tsx
+++ components/transaction/transaction-list.tsx
@@ -46,7 +46,7 @@ const TransactionList = async () => {
   const response = await
     fetch ( `${ process.env.NEXT_PUBLIC_API_URL }/transactions`, {
       next : {
-        "tags" : [
+        tags : [
           "transaction-list"
         ]
       }
