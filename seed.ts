31bbdbdea0974b162ceda884e54cf8435f3730d4
rohxn-rai <rohan.rai932@outlook.com>
2025-08-20 10:37:49 -0400

    updated seeder with new data types -made changes to category

--- seed.ts
+++ seed.ts
@@ -1,111 +1,107 @@
 import { createClient } from "@supabase/supabase-js";
-import { faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
 
 import dotenv from "dotenv";
 
-dotenv.config ( {
-  path : ".env",
-  debug : false
-} )
+dotenv.config({
+  path: ".env",
+  debug: false,
+});
 
-type TypeOfTransaction =
-  "Income" |
-  "Expense" |
-  "Investment" |
-  "Saving";
+type TypeOfTransaction = "Income" | "Expense" | "Investment" | "Saving";
 
 type CategoryOfTransaction =
-  "Housing" |
-  "Transport" |
-  "Health" |
-  "Food" |
-  "Education" |
-  "Other";
-
+  | "Housing"
+  | "Transport"
+  | "Health"
+  | "Food"
+  | "Education"
+  | "Other"
+  | "";
 
 interface Transaction {
-  created_at : Date
-  amount : number
-  type : TypeOfTransaction
-  category : CategoryOfTransaction
-  description : string
+  created_at: Date;
+  amount: number;
+  type: TypeOfTransaction;
+  category: CategoryOfTransaction;
+  description: string;
 }
 
-const supabase = createClient (
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE!
-)
+);
 
-const categories : CategoryOfTransaction[] = [
+const categories: CategoryOfTransaction[] = [
   "Housing",
   "Transport",
   "Health",
   "Food",
   "Education",
-  "Other" ]
+  "Other",
+];
 
 const seed = async () => {
-  const transactions : Transaction[] = []
-  
-  for ( let i = 0; i < 10; i++ ) {
-    const created_at = faker.date.past ()
-    
-    let type : TypeOfTransaction, category : CategoryOfTransaction = "Other"
-    
-    const typeBias = Math.random ()
-    
-    if ( typeBias < 0.80 ) {
-      type = "Expense"
-      category = faker.helpers.arrayElement ( categories )
-    } else if ( typeBias < 0.90 ) {
-      type = "Income"
+  const transactions: Transaction[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    const created_at = faker.date.past();
+
+    let type: TypeOfTransaction,
+      category: CategoryOfTransaction = "";
+
+    const typeBias = Math.random();
+
+    if (typeBias < 0.8) {
+      type = "Expense";
+      category = faker.helpers.arrayElement(categories);
+    } else if (typeBias < 0.9) {
+      type = "Income";
     } else {
-      type = faker.helpers.arrayElement ( [
-        "Saving", "Investment"
-      ] )
+      type = faker.helpers.arrayElement(["Saving", "Investment"]);
     }
-    
-    let amount : number
-    
-    switch ( type ) {
+
+    let amount: number;
+
+    switch (type) {
       case "Expense":
-        amount = faker.number.int ( {
-          min : 10,
-          max : 1000
-        } )
+        amount = faker.number.int({
+          min: 10,
+          max: 1000,
+        });
         break;
       case "Income":
-        amount = faker.number.int ( {
-          min : 2000,
-          max : 9000
-        } )
+        amount = faker.number.int({
+          min: 2000,
+          max: 9000,
+        });
         break;
       case "Investment":
       case "Saving":
       default:
-        amount = faker.number.int ( {
-          min : 3000,
-          max : 10000
-        } )
+        amount = faker.number.int({
+          min: 3000,
+          max: 10000,
+        });
         break;
     }
-    
-    transactions.push ( {
+
+    transactions.push({
       created_at,
       amount,
       type,
-      description : faker.lorem.sentence (),
-      category
-    } )
+      description: faker.lorem.sentence(),
+      category,
+    });
   }
-  
-  const { error } = await supabase.from ( "transactions" ).insert ( transactions )
-  
-  if ( error ) {
-    console.error ( error )
-  } else {
-    console.log ( "Data inserted successfully." )
-  }
-}
 
-seed ().catch ( (console.error) )
+  const { error } = await supabase.from("transactions").insert(transactions);
+
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Data inserted successfully.");
+  }
+};
+
+seed().catch(console.error);
