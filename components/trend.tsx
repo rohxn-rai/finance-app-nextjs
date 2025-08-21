30067f6bb04a80340beefdb2a49d20c72cb39da9
rohxn-rai <rohan.rai932@outlook.com>
2025-08-21 07:28:14 -0400

    replaced fetch with supabase

--- components/trend.tsx
+++ components/trend.tsx
@@ -1,16 +1,19 @@
 import BaseTrend from "@/components/base-trend";
 import { TypeOfTransaction } from "@/types/transaction";
+import { createClient } from "@/utils/supabase/server";
 
-const Trend = async ( {
-  type,
-} : {
-  type : TypeOfTransaction;
-} ) => {
-  const response = await fetch (
-    `${ process.env.NEXT_PUBLIC_API_URL }/trends/${ type }` );
-  
-  const { amount, prevAmount } = await response.json ();
-  return <BaseTrend type={ type } amount={ amount } prevAmount={ prevAmount }/>;
+const Trend = async ({ type }: { type: TypeOfTransaction }) => {
+  const supabase = await createClient();
+
+  let { data, error } = await supabase.rpc("calculate_total", {
+    type_arg: type,
+  });
+
+  if (error) console.error(error);
+
+  const amount = data ?? 0;
+
+  return <BaseTrend type={type} amount={amount} prevAmount={amount} />;
 };
 
 export default Trend;
