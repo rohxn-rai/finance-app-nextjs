ff16cef9fe02f5ecfe7110001a4d096ad9c7dda1
rohxn-rai <rohan.rai932@outlook.com>
2025-08-21 07:28:01 -0400

    added filter component to the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,34 +1,34 @@
-import { Suspense } from "react";
 import Link from "next/link";
+import { Suspense } from "react";
 
-import TransactionItemLoadingSkeleton
-  from "@/components/skeletons/transactions/transaction-list-loading-skeleton";
+import TransactionItemLoadingSkeleton from "@/components/skeletons/transactions/transaction-list-loading-skeleton";
 import TransactionList from "@/components/transaction/transaction-list";
-import TrendSetLoadingSkeleton
-  from "@/components/skeletons/trend/trend-set-loading-skeleton";
+import TrendSetLoadingSkeleton from "@/components/skeletons/trend/trend-set-loading-skeleton";
 import { Button } from "@/components/ui/button";
 
 import { PlusCircle } from "lucide-react";
+import FilterByTimeRecent from "@/components/filters/filter-by-time-dashboard";
 
 const DashboardPage = () => {
   return (
     <>
       <div className="flex flex-col gap-8">
-        <section>
-          <h1 className="text-4xl font-semibold">Summary</h1>
+        <section className="flex flex-row justify-between items-center">
+          <h1 className="text-4xl font-semibold mt-2.5">Summary</h1>
+          <FilterByTimeRecent />
         </section>
-        <TrendSetLoadingSkeleton/>
+        <TrendSetLoadingSkeleton />
         <section className="flex flex-row justify-between items-center">
           <h2 className="text-2xl">Transactions</h2>
           <Link href="/dashboard/transaction/add">
             <Button variant="outline" size="sm" className="cursor-pointer">
-              <PlusCircle className="w-4 h-4"/>
+              <PlusCircle className="w-4 h-4" />
               <span>Add</span>
             </Button>
           </Link>
         </section>
-        <Suspense fallback={ <TransactionItemLoadingSkeleton/> }>
-          <TransactionList/>
+        <Suspense fallback={<TransactionItemLoadingSkeleton />}>
+          <TransactionList />
         </Suspense>
       </div>
     </>
