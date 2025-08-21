7c709cebd4e5dac8ec29ed7fe314750565ca883d
rohxn-rai <rohan.rai932@outlook.com>
2025-08-21 10:09:31 -0400

    added the filter to calculate-total function

--- components/trend.tsx
+++ components/trend.tsx
@@ -1,19 +1,36 @@
 import BaseTrend from "@/components/base-trend";
-import { TypeOfTransaction } from "@/types/transaction";
+import {
+  TypeOfTransaction,
+  type FilterByTime,
+  type SupabaseTrendData,
+} from "@/types/transaction";
 import { createClient } from "@/utils/supabase/server";
 
-const Trend = async ({ type }: { type: TypeOfTransaction }) => {
+const Trend = async ({
+  type,
+  filter = "last30days",
+}: {
+  type: TypeOfTransaction;
+  filter: FilterByTime;
+}) => {
   const supabase = await createClient();
 
   let { data, error } = await supabase.rpc("calculate_total", {
+    range_arg: filter,
     type_arg: type,
   });
 
   if (error) console.error(error);
 
-  const amount = data ?? 0;
+  const amounts: SupabaseTrendData = data[0];
 
-  return <BaseTrend type={type} amount={amount} prevAmount={amount} />;
+  return (
+    <BaseTrend
+      type={type}
+      amount={amounts.current_amount}
+      prevAmount={amounts.previous_amount}
+    />
+  );
 };
 
 export default Trend;
