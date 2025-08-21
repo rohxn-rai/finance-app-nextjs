3e506f08f422794f0076b08f8da67de067f3c2ed
rohxn-rai <rohan.rai932@outlook.com>
2025-08-21 10:08:58 -0400

    passed the filter to trend component

--- components/skeletons/trend/trend-set-loading-skeleton.tsx
+++ components/skeletons/trend/trend-set-loading-skeleton.tsx
@@ -1,26 +1,24 @@
 import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary"
+import { ErrorBoundary } from "react-error-boundary";
 import { TYPEOFTRANSACTION } from "@/constants/all-consts";
 
-import BaseTrendLoadingSkeleton
-  from "@/components/skeletons/trend/base-trend-loading-skeleton";
+import BaseTrendLoadingSkeleton from "@/components/skeletons/trend/base-trend-loading-skeleton";
 import Trend from "@/components/trend";
 import BaseTrendError from "@/components/errors/trends/base-trend-error";
+import type { FilterByTime } from "@/types/transaction";
 
-const TrendSetLoadingSkeleton = () => {
+const TrendSetLoadingSkeleton = ({ filter }: { filter: FilterByTime }) => {
   return (
-    <section
-      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-      { TYPEOFTRANSACTION.map ( type => (
-        <ErrorBoundary key={ type }
-                       fallback={ <BaseTrendError type={ type }/> }>
-          <Suspense fallback={ <BaseTrendLoadingSkeleton type={ type }/> }>
-            <Trend type={ type }/>
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+      {TYPEOFTRANSACTION.map((type) => (
+        <ErrorBoundary key={type} fallback={<BaseTrendError type={type} />}>
+          <Suspense fallback={<BaseTrendLoadingSkeleton type={type} />}>
+            <Trend type={type} filter={filter} />
           </Suspense>
         </ErrorBoundary>
-      ) ) }
+      ))}
     </section>
-  )
-}
+  );
+};
 
 export default TrendSetLoadingSkeleton;
