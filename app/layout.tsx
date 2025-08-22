3fee5732f4a782e46a9564a7a948db82d6778898
rohxn-rai <rohan.rai932@outlook.com>
2025-08-22 16:50:41 -0400

    added header component to rootlayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,40 +3,40 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
+import Header from "@/components/header";
 
-const inter = Inter ( { subsets : [ "latin" ] } );
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata : Metadata = {
-  title : {
-    template : "%s | Finance App",
-    default : "Finance App"
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Finance App",
+    default: "Finance App",
   },
-  description : "Create and manage your finances!",
+  description: "Create and manage your finances!",
 };
 
-const RootLayout = ( {
+const RootLayout = ({
   children,
-} : Readonly<{
-  children : React.ReactNode;
-}> ) => {
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   return (
     <html lang="en" suppressHydrationWarning>
-    <body
-      className={ `${ inter.className } min-h-screen flex flex-col antialiased` }
-    >
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <main>{ children }</main>
-      <Toaster position="top-right" richColors/>
-    </ThemeProvider>
-    </body>
+      <body className={`${inter.className} flex flex-col antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main>{children}</main>
+          <Toaster position="top-right" richColors />
+        </ThemeProvider>
+      </body>
     </html>
   );
-}
+};
 
 export default RootLayout;
