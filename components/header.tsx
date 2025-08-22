c373eceef4a5170375a8a1786e7745ee8decef0d
rohxn-rai <rohan.rai932@outlook.com>
2025-08-22 15:48:14 -0400

    modified header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import { ModeToggle } from "@/components/ui/theme-toggle";
+import { cn } from "@/lib/utils";
 
-const Header = ({ className }: { className?: string }) => {
+const Header = () => {
   return (
-    <header
-      className={`flex flex-row justify-between items-center ${className}`}
-    >
-      <Link
-        href="/dashboard"
-        className="text-xl hover:underline underline-offset-8 decoration-2"
+    <header className="sticky top-0 pt-8 backdrop-blur-md mb-8">
+      <div
+        className={cn(
+          "border border-input mx-4 ",
+          "bg-popover px-4 py-2 rounded-md ",
+          "flex flex-row justify-between ",
+          "items-center ",
+          "border border-input "
+        )}
       >
-        Finance App
-      </Link>
+        <Link
+          href="/dashboard"
+          className="text-xl hover:underline underline-offset-8 decoration-2"
+        >
+          Finance App
+        </Link>
 
-      <div className="flex items-center gap-4">
-        <ModeToggle />
-        <div>User Dropdown</div>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          <div>User Dropdown</div>
+        </div>
       </div>
     </header>
   );
