ef97351cdf5fd57ff93411beb3b45d0fc9b1158b
rohxn-rai <rohan.rai932@outlook.com>
2025-08-22 15:49:52 -0400

    added ad back-button

--- app/dashboard/transaction/add/page.tsx
+++ app/dashboard/transaction/add/page.tsx
@@ -1,18 +1,27 @@
 import { Metadata } from "next";
+import Link from "next/link";
+
 import AddTransactionForm from "@/components/transaction/add-transaction-form";
 
-export const metadata : Metadata = {
-  title : "Add Transaction",
-  description : "Add your transactions to your account",
-}
+import { MoveLeft } from "lucide-react";
+
+export const metadata: Metadata = {
+  title: "Add Transaction",
+  description: "Add your transactions to your account",
+};
 
 const AddTransactionPage = () => {
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-4xl font-semibold mb-8">Add Transaction</h1>
-      <AddTransactionForm/>
+    <div className="flex flex-col gap-8 mb-8">
+      <div className="flex flex-row items-center gap-8">
+        <Link href="/dashboard">
+          <MoveLeft className="min-h-9 min-w-9" />
+        </Link>
+        <h1 className="text-4xl font-semibold">Add Transaction</h1>
+      </div>
+      <AddTransactionForm />
     </div>
-  )
-}
+  );
+};
 
-export default AddTransactionPage
+export default AddTransactionPage;
