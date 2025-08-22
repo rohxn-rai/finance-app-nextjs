819ee44f9fe36cf85eea4a90ba6df293b0e3bb24
rohxn-rai <rohan.rai932@outlook.com>
2025-08-22 08:36:38 -0400

    made changes to the trend component - used absolute value instead of "+-"

--- components/base-trend.tsx
+++ components/base-trend.tsx
@@ -3,48 +3,48 @@ import { TypeOfTransaction } from "@/types/transaction";
 import { ArrowDownLeft, ArrowUpRight } from "lucide-react";
 import useFormatCurrency from "@/hooks/use-format-currency";
 
-const BaseTrend = ( {
+const BaseTrend = ({
   type,
   amount,
   prevAmount,
-} : {
-  type : TypeOfTransaction;
-  amount : number;
-  prevAmount? : number;
-} ) => {
+}: {
+  type: TypeOfTransaction;
+  amount: number;
+  prevAmount?: number;
+}) => {
   const colorClasses = {
-    Income : "text-green-700 dark:text-green-300",
-    Expense : "text-red-700 dark:text-red-300",
-    Investment : "text-indigo-700 dark:text-indigo-300",
-    Saving : "text-yellow-700 dark:text-yellow-300",
+    Income: "text-green-700 dark:text-green-300",
+    Expense: "text-red-700 dark:text-red-300",
+    Investment: "text-indigo-700 dark:text-indigo-300",
+    Saving: "text-yellow-700 dark:text-yellow-300",
   };
-  
-  const calcPercentageChange = ( amount : number, prevAmount? : number ) => {
-    if ( !prevAmount || !amount ) return 0;
+
+  const calcPercentageChange = (amount: number, prevAmount?: number) => {
+    if (!prevAmount || !amount) return 0;
     return ((amount - prevAmount) / prevAmount) * 100;
   };
-  
-  const percentageChange = useMemo (
-    () => calcPercentageChange ( amount, prevAmount ),
-    [ amount, prevAmount ]
+
+  const percentageChange = useMemo(
+    () => calcPercentageChange(amount, prevAmount),
+    [amount, prevAmount]
   );
-  
-  const formattedAmount = useFormatCurrency ( amount );
-  
+
+  const formattedAmount = useFormatCurrency(amount);
+
   return (
     <div>
-      <div className={ `font-semibold ${ colorClasses[type] }` }>{ type }</div>
+      <div className={`font-semibold ${colorClasses[type]}`}>{type}</div>
       <div className="text-2xl font-semibold text-black dark:text-white mb-2">
-        { formattedAmount }
+        {formattedAmount}
       </div>
       <div className="flex flex-row gap-1 items-center text-sm">
-        { percentageChange <= 0 && (
-          <ArrowDownLeft className="text-red-700 dark:text-red-300"/>
-        ) }
-        { percentageChange > 0 && (
-          <ArrowUpRight className="text-green-700 dark:text-green-300"/>
-        ) }
-        { percentageChange.toFixed ( 0 ) }% vs last period
+        {percentageChange <= 0 && (
+          <ArrowDownLeft className="text-red-700 dark:text-red-300" />
+        )}
+        {percentageChange > 0 && (
+          <ArrowUpRight className="text-green-700 dark:text-green-300" />
+        )}
+        {Math.abs(percentageChange).toFixed(0)}% vs last period
       </div>
     </div>
   );
--- components/trend.tsx
+++ components/trend.tsx
@@ -1,34 +1,34 @@
 import BaseTrend from "@/components/base-trend";
 import {
-  TypeOfTransaction,
   type FilterByTime,
   type SupabaseTrendData,
+  TypeOfTransaction,
 } from "@/types/transaction";
 import { createClient } from "@/utils/supabase/server";
 
-const Trend = async ({
+const Trend = async ( {
   type,
   filter = "last30days",
-}: {
-  type: TypeOfTransaction;
-  filter: FilterByTime;
-}) => {
-  const supabase = await createClient();
-
-  let { data, error } = await supabase.rpc("calculate_total", {
-    range_arg: filter,
-    type_arg: type,
-  });
-
-  if (error) console.error(error);
-
-  const amounts: SupabaseTrendData = data[0];
-
+} : {
+  type : TypeOfTransaction;
+  filter : FilterByTime;
+} ) => {
+  const supabase = await createClient ();
+  
+  const { data, error } = await supabase.rpc ( "calculate_total", {
+    range_arg : filter,
+    type_arg : type,
+  } );
+  
+  if ( error ) console.error ( error );
+  
+  const amounts : SupabaseTrendData = data[0];
+  
   return (
     <BaseTrend
-      type={type}
-      amount={amounts.current_amount}
-      prevAmount={amounts.previous_amount}
+      type={ type }
+      amount={ amounts.current_amount }
+      prevAmount={ amounts.previous_amount }
     />
   );
 };
