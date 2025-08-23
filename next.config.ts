acc4873a0cae92b51c7de3f1c709a0d4283d679b
rohxn-rai <rohan.rai932@outlook.com>
2025-08-23 07:00:05 -0400

    added another origin for testing app on another device

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", process.env.REMOTE_TESTING!],
+    },
+  },
 };
 
 export default nextConfig;
