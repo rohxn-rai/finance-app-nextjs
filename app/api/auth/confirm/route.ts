14809c38e23e8f6730fd2c5a0b2bd4e044396ced
rohxn-rai <rohan.rai932@outlook.com>
2025-08-25 07:02:42 -0400

    create route handler for confirming the user login

---
+++ app/api/auth/confirm/route.ts
@@ -0,0 +1,27 @@
+import { type NextRequest } from "next/server";
+import { redirect } from "next/navigation";
+
+import { type EmailOtpType } from "@supabase/supabase-js";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = searchParams.get("next") ?? "/";
+
+  if (token_hash && type) {
+    const supabase = await createClient();
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
+    if (!error) {
+      redirect(next);
+    }
+  }
+
+  redirect("/error");
+}
