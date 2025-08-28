327679b6a8c4c16f3f0748b8522c306fc5febc80
rohxn-rai <rohan.rai932@outlook.com>
2025-08-28 06:45:01 -0400

    refined the settings layout

--- app/dashboard/settings/layout.tsx
+++ app/dashboard/settings/layout.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import SettingsSidebar from "@/components/dashboard/settings-sidebar";
 
 const SettingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider className="flex flex-col md:flex-row gap-2">
       <SettingsSidebar />
-      <div className="flex flex-col gap-4 md:pt-11">
-        <div className="flex flex-row items-center md:hidden">
-          <SidebarTrigger />
-          <span>User</span>
-        </div>
-        <div className="pl-2">{children}</div>
-      </div>
+      <div className="flex flex-col gap-4 md:pt-11 px-6 w-full">{children}</div>
     </SidebarProvider>
   );
 };
