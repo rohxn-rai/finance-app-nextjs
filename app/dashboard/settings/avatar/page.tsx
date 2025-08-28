fef43f6ad734a8c152752c2909d2045e98cb9f86
rohxn-rai <rohan.rai932@outlook.com>
2025-08-28 06:40:13 -0400

    added settings avatar page

---
+++ app/dashboard/settings/avatar/page.tsx
@@ -0,0 +1,12 @@
+import UploadAvatarForm from "@/components/settings/avatar-form";
+
+const AvatarSettingsPage = () => {
+  return (
+    <>
+      <h1 className="text-2xl font-semibold">Avatar</h1>
+      <UploadAvatarForm />
+    </>
+  );
+};
+
+export default AvatarSettingsPage;
