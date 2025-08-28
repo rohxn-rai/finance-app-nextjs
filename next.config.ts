543a58d59e9f898043e3a42a1fbb05d9988ef3eb
rohxn-rai <rohan.rai932@outlook.com>
2025-08-28 06:41:07 -0400

    added additional config to allow for user to send in 2mb file to a server action

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       allowedOrigins: ["localhost:3000", process.env.REMOTE_TESTING!],
+      bodySizeLimit: "2mb",
     },
   },
 };
