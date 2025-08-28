9ba3bc9c52ffd2bd19e82d0e13090239692f9a07
rohxn-rai <rohan.rai932@outlook.com>
2025-08-28 06:22:27 -0400

    added avatar image to the header

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { ModeToggle } from "@/components/ui/theme-toggle";
 
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 import LogoutButton from "@/components/logout-button";
 
 import { cn } from "@/lib/utils";
@@ -17,8 +19,21 @@ const Header = async () => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const avatarPath = user?.user_metadata?.avatar;
+  let signedUrl = "";
+
+  if (avatarPath) {
+    const { data } = await supabase.storage
+      .from("finance-avatars")
+      .createSignedUrl(avatarPath, 60 * 5);
+
+    if (data?.signedUrl) {
+      signedUrl = data.signedUrl;
+    }
+  }
+
   return (
-    <header className="sticky top-0 pt-8 backdrop-blur-md mb-8">
+    <header className="sticky top-0 pt-8 backdrop-blur-md mb-8 z-10">
       <div
         className={cn(
           "border border-input mx-4 ",
@@ -43,7 +58,16 @@ const Header = async () => {
                 variant="ghost"
                 className="flex flex-row gap-2 items-center"
               >
-                <CircleUser className="w-6 h-6" />
+                <Avatar className="rounded-full bg-popover">
+                  <AvatarImage src={signedUrl} alt={user.email} />
+                  <AvatarFallback>
+                    {signedUrl ? (
+                      <Skeleton className="w-8 h-8" />
+                    ) : (
+                      <CircleUser />
+                    )}
+                  </AvatarFallback>
+                </Avatar>
                 <span>{user?.email}</span>
               </Button>
             </Link>
