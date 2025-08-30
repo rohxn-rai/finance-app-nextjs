b86f304a33fd35d93e82d713c5abd3ca9885c227
rohxn-rai <rohan.rai932@outlook.com>
2025-08-30 14:59:44 -0400

    added user-settings data type

--- types/user-settings.ts
+++ types/user-settings.ts
@@ -1,6 +1,12 @@
 import z from "zod";
 
 import { FILTERBYTIME } from "@/constants/all-consts";
+import type { FilterByTime } from "@/types/transaction";
+
+export type GetUpdatedSetings = {
+  fullName?: string;
+  defaultView?: FilterByTime;
+};
 
 export const UserSettings = z.object({
   fullName: z.string().min(3, "Display name can not be less than 3 characters"),
