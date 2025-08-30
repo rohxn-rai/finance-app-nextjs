40d8d64aaf69f7af3c2c194839e22980f5135556
rohxn-rai <rohan.rai932@outlook.com>
2025-08-30 14:46:12 -0400

    added validation on updating user settings action

--- actions/updating-user-settings.ts
+++ actions/updating-user-settings.ts
@@ -1,14 +1,23 @@
 "use server";
 
+import { UserSettings } from "@/types/user-settings";
 import { createClient } from "@/utils/supabase/server";
 
 const UpdatingUserSettings = async (formdata: FormData) => {
   const supabase = await createClient();
 
+  const data = {
+    fullName: formdata.get("fullName"),
+    defaultView: formdata.get("defaultView"),
+  };
+
+  const validateData = UserSettings.safeParse(data);
+
+  if (!validateData.success) throw new Error("Data is not valid!");
+
   const { error } = await supabase.auth.updateUser({
     data: {
-      fullName: formdata.get("fullName"),
-      defaultView: formdata.get("defaultView"),
+      settings: data,
     },
   });
 
